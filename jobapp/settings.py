a0350a00b721fd30223fa793034f555e936b316a
Terry Gilmore <tgilly93@gmail.com>
2022-10-23 12:51:37 -0400

    postgres sql migration

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -83,13 +83,25 @@ WSGI_APPLICATION = 'jobapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+# }
 
+
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : os.getenv('DATABASE_NAME'),
+        'USER' : os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST' : os.getenv('DATABASE_HOST'),
+        'PORT' : os.getenv('DATABASE_PORT'),
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
