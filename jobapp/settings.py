6fe0f5b957a41b09200a26236907e6191e05e63d
Terry Gilmore <tgilly93@gmail.com>
2022-10-23 10:35:59 -0400

    aws integration

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'app1.apps.App1Config',
     'subscribe.apps.SubscribeConfig',
     'uploadapp.apps.UploadappConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
