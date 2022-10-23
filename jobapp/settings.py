56e054ed5f0d57e6ffda609f99b20b03f9e40828
Terry Gilmore <tgilly93@gmail.com>
2022-10-23 09:40:46 -0400

    reenabled whitenoise

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,4 +135,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- jobapp/urls.py
+++ jobapp/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('uploads/', include('uploadapp.urls')),
     
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #\
+  #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
